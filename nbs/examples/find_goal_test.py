4943f466045c1dfda99daca8ecd9486a4bc16c79
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2026-05-18 17:59:31 +0300

    ADD: grid recorder wrapper to record a video/export individual frames.

--- nbs/examples/find_goal_test.py
+++ nbs/examples/find_goal_test.py
@@ -15,7 +15,7 @@ from torchrl.envs.libs import pettingzoo
 #     render_mode='human'
 # )
 
-env = gym.make('MultiGrid-FindGoal-15x15-v0', agents=2, render_mode='human', num_obstacles=6, width=15, height=15)
+env = gym.make('MultiGrid-FindGoal-15x15-v0', agents=2, render_mode='rgb_array', num_obstacles=6, width=15, height=15)
 
 obs, info = env.reset(seed=0)
 
@@ -23,6 +23,7 @@ done = False
 while not done:
     actions = {i: env.action_space[i].sample() for i in range(env.unwrapped.num_agents)}
     obs, rewards, terminations, truncations, info = env.step(actions)
+    import ipdb; ipdb.set_trace()
     done = all(terminations.values()) or all(truncations.values())
 
 env.close()
